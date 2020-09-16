d7c19e94e185ae64fe29f35f8eb68e8e574911b7
Саша Панов <alexpanov2001@gmail.com>
2020-09-16 19:09:52 +0300

    author

---
+++ src/Author.java
@@ -0,0 +1,33 @@
+public class Author
+{
+    private String name, email;
+    private char gender;
+
+    Author(String name,String email,char gender)
+    {
+        this.name=name;
+        this.email=email;
+        this.gender=gender;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public char getGender()
+    {
+        return gender;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email=email;
+    }
+
+    public String toString(){
+        return "name " + this.name+", email "+this.email+", gender "+this.gender;
+    }
+}
